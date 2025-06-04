7214f6a09ee8121da26056e5cb3165b65849a635
Wesley Hendley <wrhendley@gmail.com>
2025-06-04 17:49:36 -0500

    Rename folder

--- derppit/config/settings.py
+++ derppit/config/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'derppit.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'derppit.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
