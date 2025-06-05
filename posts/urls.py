e203152a3d2179892290a95adfa5c012a44e6475
Wesley Hendley <wrhendley@gmail.com>
2025-06-05 13:28:32 -0500

    Update URL routing to use post_list view and remove index function

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.post_list, name='post_list'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Welcome to Derppit!")
+def post_list(request):
+    template = loader.get_template('homepage.html')
+    return HttpResponse(template.render())
