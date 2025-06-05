ccd86f3313e5a39452193807b1745fea9aa4ee06
Wesley Hendley <wrhendley@gmail.com>
2025-06-04 20:00:38 -0500

    Add initial migration and define Post and Comment models

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    # user_id = models.IntegerField()
+    # subderppit_id = models.IntegerField()
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    parent_comment = models.ForeignKey('self', null=True, blank=True, related_name='replies', on_delete=models.CASCADE)
+    content = models.TextField()
+    upvotes = models.IntegerField(default=0)
+    downvotes = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Comment on {self.post.title}' if self.post else 'Comment'
