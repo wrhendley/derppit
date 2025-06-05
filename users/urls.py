6be8438bece7e4019835a92987fd4a824e731136
Wesley Hendley <wrhendley@gmail.com>
2025-06-05 16:53:50 -0500

    Refactor URL patterns to include signup and login paths with appropriate views

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import SignUpView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
 ]
