f5526b8b3f51c30110be7ce571e05b8a00267792
Wesley Hendley <wrhendley@gmail.com>
2025-06-05 16:54:07 -0500

    Refactor index view to remove unused code and implement SignUpView class

--- users/views.py
+++ users/views.py
@@ -1,9 +1,8 @@
+from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.shortcuts import render
-from django.http import HttpResponse
 
-def index(request):
+class SignUpView(CreateView):
     form_class = UserCreationForm
     template_name = 'auth/signup.html'
     success_url = reverse_lazy('login')
