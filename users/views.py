9e4328a5061f3ba894c493906dd3d4cbe1f04b03
Wesley Hendley <wrhendley@gmail.com>
2025-06-05 15:50:48 -0500

    Refactor index view to use UserCreationForm and render signup template

--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Welcome to Derppit Users!")
+    form_class = UserCreationForm
+    template_name = 'auth/signup.html'
+    success_url = reverse_lazy('login')
