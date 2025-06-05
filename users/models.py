14fe604cbce5658de80b49be1c96e33546c42ea2
Wesley Hendley <wrhendley@gmail.com>
2025-06-04 21:47:30 -0500

    Implement user model and signals for user creation

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class DerppitUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='derppit_user')
+    avatar = models.ImageField(upload_to='avatars/', default='avatars/default.png')
+    bio = models.TextField(blank=True, null=True)
+    karma = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+    
