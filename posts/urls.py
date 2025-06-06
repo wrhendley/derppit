78629f8b27fa90ed2a899d3b5fc5a376dbe0c089
Wesley Hendley <wrhendley@gmail.com>
2025-06-06 17:53:36 -0500

    Update URL patterns to include CreatePostView for post creation

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('posts/create/', views.create_post, name='create_post'),
+    path('create_subderppit/', views.CreateSubderppitView.as_view(), name='create_subderppit'),
 ]
