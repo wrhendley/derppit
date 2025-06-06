36251376eb605f36e83e38d85a46aa448e0b4d9a
Wesley Hendley <wrhendley@gmail.com>
2025-06-06 16:24:37 -0500

    Remove app_name declaration from posts URLs and update navbar links for consistency; add logout URL pattern

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from . import views
 
-app_name = 'posts'
-
 urlpatterns = [
     path('', views.post_list, name='post_list'),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import SignUpView
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='post_list'), name='logout'),
 ]
