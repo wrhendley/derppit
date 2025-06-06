0d7f10bf140212e09c1b055bbbadbc7e77d9c5bc
Wesley Hendley <wrhendley@gmail.com>
2025-06-06 17:53:14 -0500

    Refactor views to include specific templates for post and subderppit creation, and enforce login requirements for creation views.

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .models import Post, Subderppit
+from .forms import PostForm, SubderppitForm
 
 def post_list(request):
-    return render(request, 'homepage.html')
+    return render(request, 'posts/homepage.html')
+
+def create_post(request):
+    return render(request, 'posts/create_post.html')
+
+def create_subderppit(request):
+    return render(request, 'posts/create_subderppit.html')
+
+@method_decorator(login_required, name='dispatch')
+class CreateSubderppitView(CreateView):
+    model = Subderppit
+    form_class = SubderppitForm
+    template_name = 'create_subderppit.html'
+    success_url = reverse_lazy('post_list')
+
+@method_decorator(login_required, name='dispatch')
+class CreatePostView(CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/create_post.html'
+    success_url = reverse_lazy('post_list')
